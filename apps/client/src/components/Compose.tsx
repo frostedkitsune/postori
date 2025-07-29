1f8c66873d1ccdbc7d6dacf0ef39c0fc441255f1
nextpointer <110530249+nextpointer@users.noreply.github.com>
2025-07-30 00:18:11 +0530

    feat(client): add footer button in compose copmponent

--- apps/client/src/components/Compose.tsx
+++ apps/client/src/components/Compose.tsx
@@ -1,10 +1,32 @@
-
+import  { Dialog, DialogTrigger, DialogContent, DialogTitle,DialogHeader } from "@/components/ui/dialog";
+import { Button } from "./ui/button";
+import { Pen, Image, Link, Paperclip, CaseSensitive, Trash } from "lucide-react";
 
 
 export function Compose (){
   return (
     <>
-      <h1>Compose Component</h1>
+      <Dialog>
+        <DialogTrigger><Button>
+          <Pen /> Compose
+        </Button></DialogTrigger>
+        <DialogContent className="">
+          <DialogHeader>
+            <DialogTitle>New Message</DialogTitle>
+          </DialogHeader>
+          <div className="">
+
+          </div>
+          <div className="h-16 w-full flex justify-start items-center gap-2">
+              <Button>send</Button>
+              <Button variant={"ghost"} ><CaseSensitive /></Button>
+              <Button variant={"ghost"}><Image /></Button>
+              <Button variant={"ghost"}><Link /></Button>
+              <Button variant={"ghost"}><Paperclip /></Button>
+              <Button variant={"ghost"} className=" text-red-500 ml-auto"><Trash /></Button>
+          </div>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
