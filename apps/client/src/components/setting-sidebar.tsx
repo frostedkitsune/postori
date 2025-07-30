cb1f9782497a7772e416043533c3550d2a58bc08
Amyboid <134444582+Amyboid@users.noreply.github.com>
2025-07-30 18:02:08 +0530

    feat(client): add settings page as a dialog

---
+++ apps/client/src/components/setting-sidebar.tsx
@@ -0,0 +1,59 @@
+import { Import, Sun, User } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem
+} from "@/components/ui/sidebar";
+import { Link } from "react-router";
+
+const items = [
+  {
+    title: "Profile",
+    url: "profile",
+    icon: User,
+  },
+  {
+    title: "Appearence",
+    url: "appearance",
+    icon: Sun,
+  },
+  {
+    title: "Import",
+    url: "import",
+    icon: Import,
+  },
+
+];
+
+
+export function SettingSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>General</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link to={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
