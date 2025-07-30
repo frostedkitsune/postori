31c0725e067e388c7e3a2a8ff776d1fc7d87210e
Amyboid <134444582+Amyboid@users.noreply.github.com>
2025-07-30 18:03:35 +0530

    feat(client): add appearence page inside settings

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,6 +1,15 @@
 import { RouterProvider } from 'react-router';
 import { router } from './router/routes';
+import { ThemeProvider } from "@/components/theme-provider"
 
 export default function App() {
-  return <RouterProvider router={router} />;
+
+
+  return (
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />;
+    </ThemeProvider>
+  )
+
+
 }
