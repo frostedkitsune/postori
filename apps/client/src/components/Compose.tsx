52eea4a186113e2a4a59ec3be379c38a537287c8
nextpointer <110530249+nextpointer@users.noreply.github.com>
2025-07-31 18:29:22 +0530

    feat(client): add tag input into compose component

--- apps/client/src/components/Compose.tsx
+++ apps/client/src/components/Compose.tsx
@@ -10,20 +10,26 @@ import { Pen, Image, Link, Paperclip, CaseSensitive, Trash } from "lucide-react"
 import { Textarea } from "./ui/textarea";
 import { Input } from "./ui/input";
 import { useState } from "react";
+import { TagInput } from "./TagInput";
 
 const LabelTag: React.FC<{ label: string; children?: React.ReactNode }> = ({
   label,
   children,
 }) => (
   <div className="flex flex-row items-center gap-2 mb-2">
-    <span className="w-12 text-zinc-400">{label}:</span>
-    <div className="flex-1">{children}</div>
+    <span className="w-12 text-zinc-400 mb-auto">{label}:</span>
+    {children}
   </div>
 );
 
 export function Compose() {
   const [ccState, setCcState] = useState<boolean>(false);
   const [bccState, setBccState] = useState<boolean>(false);
+
+  // state for tags
+  const [toEmails, setToEmails] = useState<string[]>([]);
+  const [ccEmails, setCcEmails] = useState<string[]>([]);
+  const [bccEmails, setBccEmails] = useState<string[]>([]);
   return (
     <Dialog>
       <DialogTrigger>
@@ -37,12 +43,13 @@ export function Compose() {
         </DialogHeader>
 
         {/* To + CC/BCC */}
+        <LabelTag label="from">
+          <span className="text-[12px] ">postori@error.party</span>
+        </LabelTag>
         <div className="flex flex-row items-start gap-2">
           <div className="flex-1">
             <LabelTag label="to">
-              <div className="">
-
-              </div>
+              <TagInput value={toEmails} onChange={setToEmails}/>
             </LabelTag>
           </div>
           <div className="flex flex-row gap-1 h-full">
@@ -56,26 +63,21 @@ export function Compose() {
         </div>
         {
           ccState && <><LabelTag label="cc">
-
+            <TagInput value={ccEmails} onChange={setCcEmails}/>
           </LabelTag></>
         }
         {
           bccState && <><LabelTag label="bcc">
-
+            <TagInput value={bccEmails} onChange={setBccEmails}/>
           </LabelTag></>
         }
-        <LabelTag label="from">
-          <div>
-
-          </div>
-        </LabelTag>
 
         <LabelTag label="subject">
-          <Input className="m-0 border-none outline-none focus:shadow-none resize-none shadow-none focus-visible:ring-0 font-semibold h-full"/>
+          <Input className="m-0 border-none outline-none focus:shadow-none shadow-none focus-visible:ring-0 font-semibold h-full"/>
         </LabelTag>
 
         <Textarea
-          className="p-0 border-none outline-none focus:shadow-none resize-none shadow-none focus-visible:ring-0 max-h-48 min-h-32"
+          className="p-0 border-none outline-none focus:shadow-none shadow-none focus-visible:ring-0 max-h-48 min-h-32"
           placeholder="..."
         />
 
@@ -93,7 +95,7 @@ export function Compose() {
           <Button variant="ghost">
             <Paperclip />
           </Button>
-          <Button variant="ghost" className="text-red-500 ml-auto">
+          <Button variant="ghost" className="text-destructive ml-auto">
             <Trash />
           </Button>
         </div>
